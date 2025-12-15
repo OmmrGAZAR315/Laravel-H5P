2a58119ec94ac7c0ab14912d9c083a292e2851a6
omar-elgazzar <omarahmedelgazzar600@gmail.com>
2025-12-15 14:33:37 +0200

    Update Laravel dependencies and refactor Blade templates for improved readability

--- views/h5p/content/embed.blade.php
+++ views/h5p/content/embed.blade.php
@@ -1,48 +1,50 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- CSRF Token -->
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <script>
-            window.Laravel = <?php echo json_encode([ 'csrfToken' => csrf_token()]); ?>
-        </script>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        {{--    core styles       --}}
-        @foreach($settings['core']['styles'] as $style)
-        {{ Html::style($style) }}
-        @endforeach
+    <script>
+        window.Laravel = <?php echo json_encode(['csrfToken' => csrf_token()]); ?>
+    </script>
 
-        @foreach($settings['loadedCss'] as $style)
-        {{ Html::style($style) }}
-        @endforeach
-    </head>
+    {{-- core styles --}}
+    @foreach($settings['core']['styles'] as $style)
+        <link rel="stylesheet" href="{{ $style }}">
+    @endforeach
 
-    <body>
+    @foreach($settings['loadedCss'] as $style)
+        <link rel="stylesheet" href="{{ $style }}">
+    @endforeach
+</head>
 
-        <div  id="app">
-            {!! $embed_code  !!}
-        </div>
+<body>
 
-        <script type="text/javascript" src="{{ url('/assets/js/app.js') }}"></script>
-        <script type="text/javascript">
-            H5PIntegration = {!! json_encode($settings) !!};
-        </script>
+    <div id="app">
+        {!! $embed_code  !!}
+    </div>
 
-        {{--    core script       --}}
-        @foreach($settings['core']['scripts'] as $script)
-        {{ Html::script($script) }}
-        @endforeach
+    <script type="text/javascript" src="{{ url('/assets/js/app.js') }}"></script>
+    <script type="text/javascript">
+        H5PIntegration = {!! json_encode($settings) !!};
+    </script>
 
-        @foreach($settings['loadedJs'] as $script)
-        {{ Html::script($script) }}
-        @endforeach
+    {{-- core script --}}
+    @foreach($settings['core']['scripts'] as $script)
+        <script src="{{ $script }}"></script>
+    @endforeach
 
-    </body>
+    @foreach($settings['loadedJs'] as $script)
+        <script src="{{ $script }}"></script>
+    @endforeach
+
+</body>
+
 </html>
